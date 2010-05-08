78605ec6e9c7d2d1c32db06625e93949ac94cdcb
Justin Knowlden <gus@gusg.us>
2010-05-07 19:13:16 -0500

    Removing application controller. Updating to 0.0.10.pre.3

--- lib/riot/action_controller.rb
+++
@@ -1,8 +0,0 @@
-require 'action_controller/test_case'
-require 'action_view/test_case'
-
-require 'riot/rails'
-require 'riot/action_controller/context_macros'
-require 'riot/action_controller/assertion_macros'
-require 'riot/action_controller/situation_macros'
-require 'riot/action_controller/context_helper'
